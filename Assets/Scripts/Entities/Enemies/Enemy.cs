2e4bc56ae353375d33d15d45603a8f060f075c70
aNDiQ <d.nikitin@xlab-game.com>
2025-12-23 17:37:11 +0300

    Add Enemy

---
+++ Assets/Scripts/Entities/Enemies/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using Entities.Enemies.Data;
+using UnityEngine;
+
+namespace Entities.Enemies
+{
+    internal class Enemy : MonoBehaviour
+    {
+        [SerializeField] private EnemyData m_enemyData;
+        private EnemyData m_data;
+
+        [SerializeField] private HealthComponent m_health;
+        // TODO: Add HealthComponent
+        // TODO: Add Movement
+        // TODO: Add AttackComponent
+
+        private void Awake()
+        {
+            Initialize(m_enemyData);
+        }
+
+        private void OnEnable()
+        {
+            m_health.ValueChanged += () =>
+            {
+                Debug.Log($"Health Changed: {m_health.Value}");
+            };
+
+            m_health.Died += OnDied;
+        }
+
+        private void OnDisable()
+        {
+            m_health.Died -= OnDied;
+        }
+
+        public void Initialize(EnemyData data)
+        {
+            m_data = data;
+            m_health.Initialize(data.health);
+        }
+
+        private void OnDied()
+        {
+            Debug.Log("Enemy Died");
+            Destroy(gameObject);
+        }
+    }
+}
